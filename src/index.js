12ef0c88f34ad0042211536808b3dd6e9e0ad670
voc0der <contact@netspace.in>
2024-12-08 23:23:57 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(
 	rateLimit({
 		windowMs: 15 * 60 * 1000,
-		max: 100,
+		max: 1000000,
 		message: "Too many requests from this IP, please try again later.",
 		standardHeaders: true,
 		legacyHeaders: false,
