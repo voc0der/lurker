7a71d683c8898e27111b632452357f4fff20e0ca
voc0der <contact@netspace.in>
2024-12-09 14:54:42 +0000

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -3,26 +3,27 @@ const { db } = require("./db");
 const { JWT_KEY } = require("./");
 
 function authenticateToken(req, res, next) {
-	if (!req.cookies || !req.cookies.auth_token) {
-		return res.redirect("/login");
-	}
+  if (!req.cookies || !req.cookies.auth_token) {
+    return res.redirect(
+      `/login?redirect=${encodeURIComponent(req.originalUrl)}`
+    );
+  }
 
-	const token = req.cookies.auth_token;
+  const token = req.cookies.auth_token;
 
-	// If no token, deny access
-	if (!token) {
-		return res.redirect(
-			`/login?redirect=${encodeURIComponent(req.originalUrl)}`,
-		);
-	}
+  if (!token) {
+    return res.redirect(
+      `/login?redirect=${encodeURIComponent(req.originalUrl)}`
+    );
+  }
 
-	try {
-		const user = jwt.verify(token, JWT_KEY);
-		req.user = user;
-		next();
-	} catch (error) {
-		res.redirect(`/login?redirect=${encodeURIComponent(req.originalUrl)}`);
-	}
+  try {
+    const user = jwt.verify(token, JWT_KEY);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.redirect(`/login?redirect=${encodeURIComponent(req.originalUrl)}`);
+  }
 }
 
 function authenticateAdmin(req, res, next) {
