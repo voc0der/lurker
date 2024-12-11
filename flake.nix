2415356ce28cd67a290719e8a133ef127ee19833
voc0der <contact@netspace.in>
2024-12-11 17:06:27 +0000

    Update Hash

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
             cp -R ./node_modules/* $out/node_modules
             ls -la $out/node_modules
           '';
-          outputHash = "sha256-rrSWV+6Xnfmq66xtZPpYARaocthoWNcZFTkN/HQVwxM=";
+          outputHash = "sha256-JkbS0DBwCJJbbaoHpkwxxumuVgl7ohu5BrAPIEV4iIg=";
           outputHashAlgo = "sha256";
           outputHashMode = "recursive";
         };
