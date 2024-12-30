16fa61528ea2895a0c80793a56093f1407eceea6
voc0der <contact@netspace.in>
2024-12-30 18:51:40 +0000

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Create group if it doesn't exist
+if ! getent group ${PGID} > /dev/null 2>&1; then
+    groupadd -g ${PGID} appgroup
+fi
+
+# Create user if it doesn't exist
+if ! id -u ${PUID} > /dev/null 2>&1; then
+    useradd -u ${PUID} -g ${PGID} -m appuser
+fi
+
+# Change ownership of the application directory
+chown -R ${PUID}:${PGID} /data /home/bun/app
+
+# Switch to the new user and execute the command
+exec gosu ${PUID}:${PGID} "$@"
