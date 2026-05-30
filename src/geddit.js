1942e5176a775e61013a66eb9a8df26cdb68228b
voc0der <contact@netspace.in>
2026-05-30 10:19:56 +0000

    Update User-Agent string in geddit.js

--- src/geddit.js
+++ src/geddit.js
@@ -11,7 +11,8 @@ class Geddit {
 			type: "sr,link,user",
 		};
 		this.headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140 Safari/537.36",
+		    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
+		    "Accept": "application/json",
 		};
 	}
 
